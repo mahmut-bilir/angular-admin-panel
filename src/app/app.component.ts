9f8de8abaa455159831410e212e4d4970f59edbc
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-05-26 13:23:35 +0300

    Refactor AppComponent to include Sidebar and Navbar components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [SidebarComponent, NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-admin-panel';
