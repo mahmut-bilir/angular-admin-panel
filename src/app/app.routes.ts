23a194d14f88d4b479f10267f3c86b01df1606b1
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-05-26 18:28:05 +0300

    Add routing configuration for DashboardComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { DashboardComponent } from './components/dashboard/dashboard.component'
+
+
+export const routes: Routes = [
+
+    { path: '', component: DashboardComponent }
+
+];
